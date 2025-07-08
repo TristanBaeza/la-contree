e96fbebdf79b532a3ceab672971f5685a3deb697
TristanBaeza <tristan.baeza@gmail.com>
2025-07-08 13:51:33 +0200

    Update pli_progress.py

--- pli_progress.py
+++ pli_progress.py
@@ -1,73 +1,115 @@
-from .constants import ATOUT_VALUES
+from .constants import ATOUT_VALUES, VALUES
 from objects import Player, Card
-from pack_creator import card_image_extractor
+from typing import Tuple, List, Union
 
 
 def pli(
-    players: list[Player],
-    pack: list | list[Card],
-    all_cards_pack: list[Card],
-    player_turn: Player, atout: str
-) -> tuple[list[Player], list[Card], Player]:
+    players: List[Player],
+    pack: Union[List, List[Card]],
+    all_cards_pack: List[Card],
+    player_turn: Player,
+    atout: str,
+) -> Tuple[List[Player], List[Card], Player]:
     cards_played_during_pli = []
     first_input = input("première carte jouée")
     first_card = input_to_card(first_input, all_cards_pack)
     index_first_player = players.index(player_turn)
     cards_played_during_pli.append(first_card)
-    for index in range(1, 4):
-        card_played = 
+    best_card = first_card
+    winner = player_turn
+    for i in range(1, 4):
+        real_index = (index_first_player + i) % 4
+        card_played = card_to_play(
+            players[real_index], all_cards_pack, cards_played_during_pli, atout
+        )
         cards_played_during_pli.append(card_played)
-        players[(index + index_first_player) % 4].deck.remove(card_played)
-
-    winner = 
+        players[real_index].deck.remove(card_played)
+        winner, best_card = determine_best_card(
+            best_card, atout, card_played, players, real_index, winner
+        )
     pack += cards_played_during_pli
-    return players, pack , winner
+    return players, pack, winner
 
 
-def input_to_card(player_input: str, all_cards_pack: list[Card]) -> Card:
+def input_to_card(player_input: str, all_cards_pack: List[Card]) -> Card:
     for card in all_cards_pack:
         if card.value == player_input[0] and card.color == player_input[1]:
             return card
     raise ValueError(f"Card {player_input} not found in the deck.")
 
 
-def card_to_play(player: Player, all_cards_pack: list[Card], cards_played_during_pli: list[Card], atout: str) -> Card:
+def card_to_play(
+    player: Player,
+    all_cards_pack: List[Card],
+    cards_played_during_pli: List[Card],
+    atout: str,
+) -> Card:
     breaker = True
-    while breaker:
-        player_input = input('carte jouée')
-        player_card = card_image_extractor(player_input[0], player_input[1])
-        
+    player_input = input("carte jouée")
+    player_card = input_to_card(player_input, all_cards_pack)
+    while not is_card_playable(
+        player, player_card, cards_played_during_pli, atout=atout
+    ):
+        player_input = input("carte jouée car erreur")
+        player_card = input_to_card(player_input, all_cards_pack)
+    return player_card
 
-def is_card_playable(player: Player, player_card: Card, cards_played_during_pli: list[Card], atout: str) -> bool:
+
+def is_card_playable(
+    player: Player, player_card: Card, cards_played_during_pli: List[Card], atout: str
+) -> bool:
     if not player_card in player.deck:
         return False
-    if cards_played_during_pli[0].color == atout:
-        return is_atout_higher_when_atout_played_first(player, player_card, cards_played_during_pli, atout)
-    if cards_played_during_pli[0].color == player_card.color:
+    colors = [card.color for card in player.deck]
+    if cards_played_during_pli[0].color in colors:
+        if player_card.color != cards_played_during_pli[0].color:
+            return False
         return True
-    if 
+    if atout in colors:
+        if player_card.color != atout:
+            return False
+        return True
+    return True
 
 
-def is_atout_higher_when_atout_played_first(player: Player, player_card: Card, cards_played_during_pli: list[Card], atout: str) -> bool:
-    number_of_atout = sum(1 for card in player.deck if card.color == atout)
-    if number_of_atout == 0:
-        return True
-    if player_card.color != atout:
-        return False
-    if number_of_atout == 1 : 
-        return True
-    index_max_played = ATOUT_VALUES.index(cards_played_during_pli[0].value)
-    index_max_player = 0
+def is_atout_higher_when_atout_played(
+    player: Player, player_card: Card, cards_played_during_pli: List[Card], atout: str
+) -> bool:
+    height_cards = []
+    player_card_height = ATOUT_VALUES.index(player_card.value)
     for card in cards_played_during_pli:
-        if ATOUT_VALUES.index(card.value) > index_max_played:
-            index_max_played = ATOUT_VALUES.index(card.value)
-    if ATOUT_VALUES.index(player_card.value) > index_max_played:
+        if card.color == atout:
+            height_cards.append(ATOUT_VALUES.index(card.value))
+    if height_cards == []:
         return True
-    for card in player.deck:
-        if ATOUT_VALUES.index(card.value) > index_max_player:
-            index_max_player = ATOUT_VALUES.index(card.value)
-    if index_max_player < index_max_played:
+    if max(height_cards) > player_card_height:
+        for card in player.deck:
+            if max(height_cards) < ATOUT_VALUES.index(card.value):
+                return False
         return True
-    return False
-    
-    
+    return True
+
+
+def determine_best_card(
+    best_card: Card,
+    atout: str,
+    card_played: Card,
+    players: List[Player],
+    index: int,
+    winner: Player,
+) -> Tuple[Player, Card]:
+    if best_card.color != atout and card_played.color == atout:
+        best_card = card_played
+        winner = players[index]
+    elif best_card.color == card_played.color:
+        if best_card.color == atout:
+            if ATOUT_VALUES.index(best_card.value) < ATOUT_VALUES.index(
+                card_played.value
+            ):
+                best_card = card_played
+                winner = players[index]
+        else:
+            if VALUES.index(best_card.value) < VALUES.index(card_played.value):
+                best_card = card_played
+                winner = players[index]
+    return winner, best_card
